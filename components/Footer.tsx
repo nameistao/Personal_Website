4262b75d182cb6cf4bfe8d9ce2e449677fc9aeac
tao luo <tao.luo@fabric.inc>
2021-10-08 13:48:39 -0700

    add social icons to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,47 @@
 import styled from "styled-components";
+import LinkedIn from "icons/LinkedIn";
+import GitHub from "icons/GitHub";
+import Email from "icons/Email";
 
 const StyledFooter = styled.footer`
   width: 100vw;
   height: 15vh;
   background: purple;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledPageLink = styled.div`
+  height: 50%;
+  width: 15vw;
+  margin-top: auto;
+  margin-bottom: auto;
+  text-align: center;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+  display: flex;
+  color: white;
+  font-size: 1.5rem;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const Footer = () => {
-  return <StyledFooter />;
+  return (
+    <StyledFooter>
+      <StyledPageLink>
+        <LinkedIn />
+      </StyledPageLink>
+      <StyledPageLink>
+        <GitHub />
+      </StyledPageLink>
+      <StyledPageLink>
+        <Email />
+      </StyledPageLink>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,10 +4,35 @@ const StyledHeader = styled.header`
   width: 100vw;
   height: 15vh;
   background: blue;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledPageLink = styled.div`
+  height: 50%;
+  width: 15vw;
+  margin-top: auto;
+  margin-bottom: auto;
+  text-align: center;
+  align-content: center;
+  justify-content: center;
+  flex-direction: column;
+  display: flex;
+  color: white;
+  font-size: 1.5rem;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const Header = () => {
-  return <StyledHeader />;
+  return (
+    <StyledHeader>
+      <StyledPageLink>About</StyledPageLink>
+      <StyledPageLink>Work</StyledPageLink>
+      <StyledPageLink>Projects</StyledPageLink>
+    </StyledHeader>
+  );
 };
 
 export default Header;
