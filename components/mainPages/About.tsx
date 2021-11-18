3dfc3d34b08e54bebb9f732d8eafc70707a2f016
tao luo <taoluo@usc.edu>
2021-11-18 08:55:34 -0800

    update about to include company link

--- components/mainPages/About.tsx
+++ components/mainPages/About.tsx
@@ -13,7 +13,19 @@ const StyledAbout = styled.div`
 const About = () => {
   return (
     <StyledAbout>
-      <p>{"my name is tao luo, and i'm a software enginner at fabric."}</p>
+      <p>
+        my name is tao luo, and i'm a software enginner at{" "}
+        <strong>
+          <a
+            href="https://fabric.inc/"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            fabric
+          </a>
+        </strong>
+        .
+      </p>
       <p>previously, i studied computer science and business at usc.</p>
     </StyledAbout>
   );
